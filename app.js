72514df665fc178a48388f8fdcb22bf75a0664f6
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-05-25 19:38:08 +0300

    feat add solution interface

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
-const URL = "http://great.jokes/christmas";
 // solution 1
 
-const sayJoke = async (apiUrl, jokeId) => {
-  let joke = await fetch(apiUrl)
-    .then((response) => response.json())
-    .then(({jokes}) => jokes.find((joke) => joke.id === jokeId))
-    .catch((err) => {
-      throw new Error(`No jokes at url: ${apiUrl}`);
-    });
+// const isSantaClausable=obj=>
+//    ["sayHoHoHo", "distributeGifts", "goDownTheChimney"]
+//      .every(item=>typeof obj[item] == "function")
 
-  if (!joke) {
-    throw new Error(`No jokes found id: ${jokeId}`);
-  }
-
-  return {
-    saySetup: () => joke.setup,
-    sayPunchLine: () => joke.punchLine,
-  };
-};
 // solution 2
 
-// async function sayJoke(apiUrl, jokeId) {
-//   let response = await fetch(apiUrl);
-//   let json = await response.json();
-//   if (!json.jokes) throw new Error(`No jokes at url: ${apiUrl}`);
+const isSantaClausable = (obj) =>
+  "function" ==
+  typeof (obj.sayHoHoHo && obj.distributeGifts && obj.goDownTheChimney);
+// ==================================
 
-//   let jokes = json.jokes;
-//   let joke = jokes.find(el => el.id === jokeId);
-//   if (!joke) throw new Error(`No jokes found id: ${jokeId}`);
+var santa = {
+  sayHoHoHo: function () {
+    console.log("Ho Ho Ho!");
+  },
+  distributeGifts: function () {
+    console.log("Gifts for all!");
+  },
+  goDownTheChimney: function () {
+    console.log("*whoosh*");
+  },
+};
 
-//     joke.saySetup = function () {
-//       return this.setup;
-//     }
+var notSanta = {
+  sayHoHoHo: function () {
+    console.log("Oink Oink!");
+  },
+  // no distributeGifts() and no goDownTheChimney()
+};
 
-//     joke.sayPunchLine = function () {
-//       return this.punchLine;
-//     }
+isSantaClausable(santa); // must return TRUE
 
-//     return joke;
-// }
+console.log(isSantaClausable(santa));
+isSantaClausable(notSanta);
 
-const joke = sayJoke("http://great.jokes/christmas", 10);
+console.log(isSantaClausable(notSanta));
